fdf41deaa425d6bb01f5da12c33ba005ef2dd964
Damien JARRY <dams_terdam@hotmail.fr>
2018-10-07 13:59:45 +0200

    WIP - Update docker and remove codemirror

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,50 +8,7 @@ import {Routing} from './app.routing';
 import {SearchComponent} from './components/search/search.component';
 import {FormsModule} from '@angular/forms';
 import {ToastModule} from './modules/toast/toast.module';
-import {CodemirrorModule} from '@ctrl/ngx-codemirror';
-import * as CodeMirror from 'codemirror';
 import {StylishTextareaModule} from './modules/stylish-textarea/stylish-textarea.module';
-
-// TODO: move that anywhere else.
-CodeMirror.defineMode('jwt', () => {
-  return {
-    startState: () => {
-      return {
-        headerFound: false,
-        payloadFound: false,
-        signatureFound: false
-      };
-    },
-    token: (stream, state) => {
-      if (stream.next() === '.') {
-        return null;
-      }
-
-      if (!state.headerFound) {
-        stream.next();
-        if (stream.skipTo('.')) {
-          state.headerFound = true;
-        } else {
-          stream.skipToEnd();
-        }
-        return 'keyword';
-      }
-
-      if (!state.payloadFound) {
-        stream.next();
-        if (stream.skipTo('.')) {
-          state.payloadFound = true;
-        } else {
-          stream.skipToEnd();
-        }
-        return 'number';
-      }
-
-      stream.skipToEnd();
-      return 'string';
-    }
-  };
-});
 
 @NgModule({
   declarations: [
@@ -67,8 +24,7 @@ CodeMirror.defineMode('jwt', () => {
     FormsModule,
     Routing,
     ToastModule,
-    StylishTextareaModule,
-    CodemirrorModule
+    StylishTextareaModule
   ],
   providers: [],
   bootstrap: [AppComponent]
